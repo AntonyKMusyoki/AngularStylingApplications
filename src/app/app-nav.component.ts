a328ca1be6b7b4ac0a70f5630af816c0cba36ae7
Antony Kyalo Musyoki <antony.kyalo@bookinghub.com>
2025-12-30 00:05:50 -0600

    Ch03/Component Styles: StyleUrls

--- src/app/app-nav.component.ts
+++ src/app/app-nav.component.ts
@@ -4,23 +4,24 @@ import { Component, ViewEncapsulation } from '@angular/core';
     selector: 'saa-app-nav',
     // encapsulation: ViewEncapsulation.None,
     template: `
-        <div style="background: #2A9FBC; border-radius: 0.5em; margin: 1.5em 0; padding: 1em 1.2em;">
-            <ul style="display: flex; list-style: none; margin: 0; padding: 0;">
+        <div>
+            <ul>
                 <li>
-                    <a href="#" style="color: #fff; padding: 0 1em; text-decoration: none;">Home</a>
+                    <a href="#">Home</a>
                 </li>
                 <li>
-                    <a href="#" style="color: #fff; padding: 0 1em; text-decoration: none;">Prerequisites</a>
+                    <a href="#">Prerequisites</a>
                 </li>
                 <li>
-                    <a href="#" style="color: #fff; padding: 0 1em; text-decoration: none;">Modules</a>
+                    <a href="#">Modules</a>
                 </li>
                 <li>
-                    <a href="#" style="color: #fff; padding: 0 1em; text-decoration: none;">About</a>
+                    <a href="#">About</a>
                 </li>
             </ul>
         </div>
     `,
+    styleUrls: ['./app-nav.component.css'],
     standalone: false
 })
 
